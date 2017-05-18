cf78b644d36109557175e8eb7875192c15d06b82
Rathersilly <rathersilly@live.com>
2017-05-18 06:07:49 -0600

    basic game done except ties

---
+++ board.rb
@@ -0,0 +1,102 @@
+
+class Board
+  def initialize
+    # @game_array = [["1|2|3"],
+    #                ["-----"],
+    #                [" | | "],
+    #                ["-----"],
+    #                [" | | "]]
+    @move_array = Array.new(2)
+    @game_array = Array.new(3){Array.new(3)}
+    square = 1
+    row = ""
+    5.times do |i|
+      row = ""
+      if i.even?
+        
+        3.times do |j|
+          row += square.to_s
+          square += 1
+          row += "|" if j < 2   
+        end
+        
+      else
+        row = "-----"
+      end
+      @move_array[i] = row
+      
+      3.times do |i|
+        3.times do |j|
+          @game_array[i][j] = " "
+        end
+      end
+    end
+
+  end
+
+  def update(player, move)
+    move = move.to_i
+    row  = (move-1)/3
+    col = (move%3) - 1
+    col = 2 if col == -1
+   
+
+    
+    @game_array[row][col] = player.sigil
+  end
+
+  def win_status
+    #check horizontal
+    #use regex
+    pattern = /(\w)\1\1/
+    
+    diag1 = ""
+    diag2 = ""
+    3.times do |i|
+      test_string = @game_array[i].join
+      
+      return true if pattern.match?(test_string)
+      
+      test_string = ""
+      
+      3.times do |j|
+        # 00 11 22    02 11 20
+        # i = j       i + j = 2
+        test_string += @game_array[j][i]
+        
+        diag1 += @game_array[i][j] if i == j
+        diag2 += @game_array[i][j] if i + j == 2
+     
+      end
+      # puts "diag1: #{diag1}"
+      # puts "diag2: #{diag2}"
+      if pattern.match?(test_string) || pattern.match?(diag1) || pattern.match?(diag2)
+       return true         
+      end       
+       
+    end
+
+    return false
+  end
+
+
+  
+
+
+
+  def display
+    #puts @game_array
+    #puts @move_array
+    3.times do |i|
+      3.times do |j|
+         print @game_array[i][j]
+         print "|" if j < 2
+      
+      end
+      print "\n"
+      puts "-----" if i < 2
+    end
+
+  end
+
+end
---
+++ tictactoe.rb
@@ -0,0 +1,86 @@
+require_relative 'board'
+#possible classes
+#board
+#player
+#game
+#
+#
+class Game
+  @@games_played = 0
+
+  def initialize(player1, player2)
+    puts "Buckle up for some Tic Tac Toe carnage!"
+    @board = Board.new
+    @player = [player1, player2]
+  end
+
+  def start
+    puts "game started"
+    index = 1
+    #while game not over
+    @board.display
+    game_won = false
+    winner = 0
+    while !game_won # and board not filled
+      index += 1
+      index = 0 if index == 2 
+      move(@player[index])
+      
+      
+      @board.display
+      game_won = @board.win_status
+      if game_won
+        winner = @player[index]
+      end
+        
+       #find current player to move
+    end
+
+    end_game(winner)
+    #get players move
+    #update board
+  end
+
+  def move(player)
+    puts "#{player.name}'s move?"
+    move_square = gets.chomp
+    @board.update(player, move_square)
+  end
+
+  def end_game(winner)
+    puts "Game over!"
+    if(winner)
+      
+      puts "#{winner.name} won!"
+    else
+      puts "It's a tie!"
+    end
+  end
+
+end
+
+class Player
+  attr_accessor :name, :sigil
+
+  @@player_number = 0
+
+  def initialize(name)
+    @name = name
+    @@player_number += 1
+   if @@player_number == 1
+    @sigil = "X"
+  else 
+    @sigil = "O"
+  end
+    #if @@player_number > 2
+      #dont allow creation
+  end
+end
+
+player1 = Player.new("Mortimer")
+player2 = Player.new("Horace")
+
+game = Game.new(player1, player2)
+game.start
+
+
